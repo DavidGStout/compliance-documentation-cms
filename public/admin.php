0b27e1ac195ba52187b4b4feddde89b9bd608dc4
DavidGStout <davidgstout63@gmail.com>
2018-02-09 13:49:52 -0500

    Created PHP files, and folders for JavaScript and CSS.

---
+++ public/admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Compliance Documentation</title>
+</head>
+<body>
+
+	<div id="header">
+		<h1>Compliance Documentation</h1>
+	
+	<div id="main"></div>
+		<div id="navigation">
+			&nbsp;
+		</div>
+
+		<div id="page">
+			<h2>Admin Menu</h2>
+			<p>Welcome to the Compliance admin area.</p>
+			<ul>
+				<li><a href="manage_content.php">Manage Content</a></li>
+				<li><a href="manage_admins.php">Manage Users</a></li>
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="javascripts/scripts.js"></script>
+	<link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+</head>
+<body>
+
+	<h1>lorem test</h1>
+
+</body>
+</html>
